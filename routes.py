fcdc3958fe84b6336fd740b822ee675341281786
kramit1116 <kramit1116@gmail.com>
2026-01-05 22:29:14 +0530

    added register and login post routes and some controller part

--- routes.py
+++ routes.py
@@ -1,4 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for,flash
+from models import db, User, Product, Category, Order, Cart, Transaction
+from werkzeug.security import generate_password_hash,check_password_hash
 from app import app
 
 @app.route('/')
@@ -9,6 +11,43 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/login', methods=['POST'])
+def login_post():
+    username = request.form['username']
+    password = request.form['password']
+    
+    if not username or not password:
+        flash('Please enter both username and password.')
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        flash('User does not exist.')
+    if not check_password_hash(user.passhash, password):
+        flash('Incorrect password.')
+    return redirect(url_for('index'))
+
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def register_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    confirm_password = request.form.get('confirm-password')
+    name = request.form.get('name')
+    if not username or not password or not confirm_password:
+        flash('Please fill out all required fields.')
+        return redirect(url_for('register'))
+    if password != confirm_password:
+        flash('Password do not match.')
+        return redirect(url_for('register'))
+    user = User.query.filter_by(username=username).first()
+    if user:
+        flash('User already exists.')
+        return redirect(url_for('register'))
+    passhash = generate_password_hash(password)
+    new_user = User(username=username, passhash=passhash, name=name)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('login'))
+
