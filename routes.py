e52440d0c2097936f193fba90042af51b9b14ba9
kramit1116 <kramit1116@gmail.com>
2026-02-04 22:39:08 +0530

    added export to csv of orders

--- routes.py
+++ routes.py
@@ -4,6 +4,8 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from functools import wraps
 from datetime import datetime
 from app import app
+import csv
+from uuid import uuid4
 
 @app.route('/login')
 def login():
@@ -445,3 +447,25 @@ def checkout():
 def orders():
     transactions = Transaction.query.filter_by(user_id=session['user_id']).order_by(Transaction.datetime.desc()).all()
     return render_template('orders.html', transactions=transactions)
+
+@app.route('/export_csv')
+@auth_required
+def export_csv():
+    user = User.query.get(session['user_id'])
+    transactions = Transaction.query.filter_by(user_id=user.id).all()
+    filename = uuid4().hex + '.csv'
+    url = 'static/csv/' + filename
+    with open(url, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Transaction ID', 'DateTime','Product Name', 'Quantity', 'Price'
+        ])  # Write header
+        for transaction in transactions:
+            for order in transaction.orders:
+                writer.writerow([
+                    transaction.id,
+                    transaction.datetime,
+                    order.product.name,
+                    order.quantity,
+                    order.price
+                ])
+    return redirect(url_for('static', filename='csv/' + filename))            
